fa21fedc9b44b6c9c76db7e0c6296fcb986c8128
hgaziuddin <hgaziuddin@students.mchenry.edu>
2016-09-08 21:52:17 -0500

    Add files via upload

---
+++ usingTurtle.py
@@ -0,0 +1,14 @@
+import turtle
+myTurtle = turtle.Turtle()
+print(myTurtle)
+
+myTurtle.fd(100)
+myTurtle.lt(90)
+myTurtle.fd(100)
+myTurtle.lt(90)
+myTurtle.fd(100)
+myTurtle.lt(90)
+myTurtle.fd(100)
+#turtle.done (do i put it here)
+turtle.mainloop()
+#turtle.done (or here?)

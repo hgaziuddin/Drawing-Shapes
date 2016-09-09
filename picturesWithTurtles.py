b19102c1f117a2593c241f064469d29b966b3bcf
hgaziuddin <hgaziuddin@students.mchenry.edu>
2016-09-08 21:26:33 -0500

    Add files via upload

---
+++ picturesWithTurtles.py
@@ -0,0 +1,38 @@
+import turtle
+theTurtle = turtle.Turtle()
+print(theTurtle)
+
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+#left side
+theTurtle.fd(50)
+theTurtle.rt(90)
+theTurtle.fd(50)
+theTurtle.rt(90)
+theTurtle.fd(50)
+theTurtle.lt(90)
+#right side
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(100)
+theTurtle.lt(90)
+theTurtle.fd(50)
+
+
+
+
+turtle.mainloop()
